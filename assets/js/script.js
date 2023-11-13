a03cefdb1f9ff2be8dcfa4d05e4edcf96e7bd839
AkshaySharma777 <jsakshay653@gmail.com>
2023-11-13 09:50:50 +0530

    completed a part of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,10 @@
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
-var box3 = document.getElementById("box3");
-var box4 = document.getElementById("box4");
-var box5 = document.getElementById("box5");
-var box6 = document.getElementById("box6");
-var box7 = document.getElementById("box7");
-var box8 = document.getElementById("box8");
-var box9 = document.getElementById("box9");
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-var bx = [];
 function game() {
   var count = 0;
   var boxes = document.querySelectorAll(".box");
-  var reset = document.getElementById("reset");
- 
 
   boxes.forEach(function (box) {
-    
-    
-    reset.addEventListener("click", function () {
-      box.innerHTML = " ";
-    });
-
     box.addEventListener("click", function () {
       if (box.innerHTML.length == 0) {
         if (count % 2 == 0) {
@@ -31,16 +14,26 @@ function game() {
           box.innerHTML = "O";
           count++;
         }
-        // bx.push(box.getAttribute("id"));
       }
      
-        if(box1.innerHTML=="X" && box2.innerHTML=="X" && box3.innerHTML=="X"){
-          alert("Success");
-     
-      }
-     
+
+        
     });
   });
   
+  boxes.forEach(function (box) {
+    // alert(box.getAttribute("id"));
+    let resultCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < board.length; i++) {
+      board[i] = box.getAttribute("id");
+      
+    }
+  });
+  console.log(`Present Array = ${board}`)
 }
 game();
+
+reset.addEventListener('click', () => {
+  location.reload();
+});
+ 
